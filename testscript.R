51c579d07daebd15d1f5dda692806ef75da22e25
William Fuh <wlfuh@umich.edu>
2016-10-24 14:59:52 -0400

    adsf

--- testscript.R
+++ testscript.R
@@ -1,1 +1,11 @@
-testdata <- read.table("test.dat", header=TRUE)
+testdata <- read.table("test.dat", header=FALSE)
+moddata <- read.table("test_modified.dat", header=FALSE)
+
+testraw <- testdata[4]
+
+modraw <- moddata[4]
+
+testmatrix <- data.matrix(testraw)
+modmatrix <- data.matrix(modraw)
+print(hausdorff_dist(testmatrix, modmatrix))
+# install.packages : pracma
