3dae7d33603aa1aa77de6b86fca03cb8ca780bcf
atfrank <afrankz@umich.edu>
2016-10-26 15:36:10 -0400

    slight updates...

--- testscript.R
+++ testscript.R
@@ -1,25 +1,36 @@
 library(pracma)
 
-testdata <- read.table("test.dat", header=FALSE)
-moddata <- read.table("test_modified.dat", header=FALSE)
+refdata <- read.table("test.dat", header=FALSE)
+colnames(refdata) <- c("resname","resid","nucleus","cs","error")
+subid <- c(1,2,3)
 
-testraw <- testdata[4]
+testdata <- refdata[!(refdata$resid %in% subid),"cs"]
+moddata <- refdata[(refdata$resid %in% subid),"cs"]
+testmatrix <- data.matrix(testdata)
+modmatrix <- data.matrix(moddata)
+hausdorff_dist(moddata, testmatrix)
+stop()
 
-modraw <- moddata[4]
-testmatrix <- data.matrix(testraw)
-modmatrix <- data.matrix(modraw)
 print(hausdorff_dist(testmatrix,jitter(modmatrix, amount = 10)))
-# install.packages : pracma
-
 
 # determine the properities of the hausdorff_dist as we data more noise to the subset
+# and make plot
 final_dist <- NULL
-for (m in seq(1,100,1)){
+ms <- seq(1,100,1)
+for (m in ms){
   # average distance
   tmp_dist <- NULL
   for (i in 1:100){
     tmp_dist <- c(tmp_dist, hausdorff_dist(testmatrix,jitter(modmatrix, amount = m)))
   }
+  final_dist <- c(final_dist, mean(tmp_dist))
 }
+
+
+
+
+# try using hungrian algorithm
+
+
 
 
